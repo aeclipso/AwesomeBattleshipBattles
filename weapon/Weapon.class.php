16045e39be9a39fb976d57a69c75856e68533f01
efremzhenya <efremzhenya@yandex.ru>
2020-06-12 12:50:56 +0300

    ship_4_fix weapon.class.php

--- weapon/Weapon.class.php
+++ weapon/Weapon.class.php
@@ -2,11 +2,23 @@
 
 class Weapon
 {
+    public $id;
     private $_name;
+    private $_distanceShort;
+	private $_distanceMedium;
+    private $_distanceLong;
+    private $_charge;
+    private $_zone;
 
-    function __construct($name)
+    function __construct(int $id, string $name, int $short, int $medium, int $long, int $charge, $zone)
     {
         $this->setName($name);
+        $this->id = $id;
+        $this->_distanceShort = $short;
+        $this->_distanceMedium = $medium;
+        $this->_distanceLong = $long;
+        $this->_zone = $zone;
+        $this->_charge = $charge;
     }
     public function setName($name)
     {
