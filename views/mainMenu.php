3f5d991d1782a5e93dca893b412fbcbdb31d36b3
bmangust <ftf.pollux@gmail.com>
2020-06-12 07:10:33 +0300

    game_7_main logic and views

---
+++ views/mainMenu.php
@@ -0,0 +1,7 @@
+<section>
+<h1>Welcome to <span class="uppercase">Awesome Battleship Battle</span></h1>
+    <form class="mainMenu" action="index.php" method="get">
+        <input class="button menuItem" type="submit" name="newGame" value="new game"/>
+        <!-- <input class="button_mainMenu" type="submit" name="settings" value="settings"/> -->
+    </form>
+</section>
